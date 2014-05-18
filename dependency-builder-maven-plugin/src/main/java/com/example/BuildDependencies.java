2eed3c0f91b0d32f425acaa680dda3b6c40f938c
Andrew Schurman <arcticwaters@gmail.com>
2014-05-17 19:55:36 -0700

    Initial commit

---
+++ dependency-builder-maven-plugin/src/main/java/com/example/BuildDependencies.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+@Mojo(name = "build-dependencies")
+public class BuildDependencies extends AbstractMojo {
+	public void execute() throws MojoExecutionException {
+	}
+}
