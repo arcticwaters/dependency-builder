32bfef04cdb26e3e84c4cca3670112f9e69b8d3d
Andrew Schurman <arcticwaters@gmail.com>
2014-05-17 22:26:21 -0700

    Configure checkstyle for the project

--- dependency-builder-maven-plugin/src/main/java/com/example/BuildDependencies.java
+++ dependency-builder-maven-plugin/src/main/java/com/example/BuildDependencies.java
@@ -4,8 +4,12 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 
+/** Builds the dependencies of a project which deploys Maven metadata. */
 @Mojo(name = "build-dependencies")
 public class BuildDependencies extends AbstractMojo {
+
+	@Override
 	public void execute() throws MojoExecutionException {
+		// TODO Auto-generated method stub
 	}
 }
